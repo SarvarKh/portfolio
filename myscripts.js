61de79447dec8f23af77b85c5b0091faefd33cbc
Sarvar Khalimov <khalimovsarvar@gmail.com>
2021-04-25 16:10:44 +0500

    Add basic html and js code for 3 modal box

--- myscripts.js
+++ myscripts.js
@@ -53,3 +53,33 @@ window.onclick = function(event) {
     modal2.style.display = "none";                       // EDIT 1!!!!!!!!!!!!!!!!!!!!!!
   }
 }
+
+
+
+
+
+// Get the modal
+var modal3 = document.getElementById("myModal3");
+
+// Get the button that opens the modal
+var btn3 = document.getElementById("myBtn3");
+
+// Get the <span> element that closes the modal
+var span3 = document.getElementById("close3");
+
+// When the user clicks the button, open the modal 
+btn3.onclick = function() {
+  modal3.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span3.onclick = function() {
+  modal3.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal3) {
+    modal3.style.display = "none";
+  }
+}
