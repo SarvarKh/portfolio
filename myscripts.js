9f2e0de0b48c7c7d50d15f4b7aa98321d269a6a1
Sarvar Khalimov <khalimovsarvar@gmail.com>
2021-04-25 18:44:11 +0500

    Add basic html and js code for 4 modal box

--- myscripts.js
+++ myscripts.js
@@ -83,3 +83,33 @@ window.onclick = function(event) {
     modal3.style.display = "none";
   }
 }
+
+
+
+
+
+// Get the modal
+var modal4 = document.getElementById("myModal4");
+
+// Get the button that opens the modal
+var btn4 = document.getElementById("myBtn4");
+
+// Get the <span> element that closes the modal
+var span4 = document.getElementById("close4");
+
+// When the user clicks the button, open the modal 
+btn4.onclick = function() {
+  modal4.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span4.onclick = function() {
+  modal4.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal4) {
+    modal4.style.display = "none";
+  }
+}
