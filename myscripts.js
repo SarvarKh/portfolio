2529d4931bf7e3c3c7aa634126c6acd9857fc32d
Sarvar Khalimov <khalimovsarvar@gmail.com>
2021-04-25 19:20:58 +0500

    Complete modal boxes 4 and 5

--- myscripts.js
+++ myscripts.js
@@ -113,3 +113,33 @@ window.onclick = function(event) {
     modal4.style.display = "none";
   }
 }
+
+
+
+
+
+// Get the modal
+var modal5 = document.getElementById("myModal5");
+
+// Get the button that opens the modal
+var btn5 = document.getElementById("myBtn5");
+
+// Get the <span> element that closes the modal
+var span5 = document.getElementById("close5");
+
+// When the user clicks the button, open the modal 
+btn5.onclick = function() {
+  modal5.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span5.onclick = function() {
+  modal5.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal5) {
+    modal5.style.display = "none";
+  }
+}
