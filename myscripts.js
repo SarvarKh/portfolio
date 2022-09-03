f363a6aef5a91ebe2e3498cf28ce83d3fd23f4bf
SarvarKhalimov <khalimovsarvar@gmail.com>
2022-09-03 20:58:47 +0500

    Add 6th project to portfolio

--- myscripts.js
+++ myscripts.js
@@ -152,3 +152,29 @@ window.onclick = function(event) {
     modal5.style.display = "none";
   }
 }
+
+// Get the modal
+var modal6 = document.getElementById("myModal6");
+
+// Get the button that opens the modal
+var btn6 = document.getElementById("myBtn6");
+
+// Get the <span> element that closes the modal
+var span6 = document.getElementById("close6");
+
+// When the user clicks the button, open the modal 
+btn6.onclick = function() {
+  modal6.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span6.onclick = function() {
+  modal6.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal6) {
+    modal6.style.display = "none";
+  }
+}
